58acc6bdb5cc962dbbf21122b13c6a52abb69421
Douae sb <douae.sb411@gmail.com>
2024-12-02 12:39:11 +0100

    intiliaze project

---
+++ src/main/java/com/prod/producia/ProduciaApplication.java
@@ -0,0 +1,13 @@
+package com.prod.producia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProduciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProduciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prod/producia/ProduciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prod.producia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProduciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
